b34be9f3c5c368edb960e71237b3ad14a1f3d0e9
maru8605 <maru8605@hotmail.com>
2021-07-23 18:39:51 -0300

    carga condicional de componente

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import {useState} from 'react'
 import Pregunta from "./component/Pregunta";
+import Formulario from './Formulario';
 
 function App() { 
 
   const [presupuesto, setPresupuesto] = useState(0);
   const [restante, setRestante] = useState(0);
+  const [verpregunta, setPregunta] = useState(true);
 
   return (
     <div className0='container'>
       <header>
         <h1>Presupuesto</h1>
         <div className='contenido-principal contenido'>
-          <Pregunta
-            setPresupuesto={setPresupuesto}
-            setRestante={setRestante}
-          />
-        </div>
-        
+          { verpregunta ?  (
+            <Pregunta
+              setPresupuesto={setPresupuesto}
+              setRestante={setRestante}
+              setPregunta={setPregunta}
+            />
+          ) : (
+            <div className='row'>
+              <div className='one-half column'>
+                <Formulario/>
+              </div>
+              <div className='one-half column'>
+              2
+              </div>
+            </div>
+          ) }    
+        </div>  
       </header>
     </div>
     
